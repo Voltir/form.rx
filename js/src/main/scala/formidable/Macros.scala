f596d3fb04e49f4d0435382d6b35b435536a746c
Nick <voltir42@gmail.com>
2014-12-26 17:30:08 -0800

    kind of better

--- js/src/main/scala/formidable/Macros.scala
+++ js/src/main/scala/formidable/Macros.scala
@@ -1,75 +1,82 @@
 package formidable
 
 import scala.reflect.macros._
+import scala.language.experimental.macros
 import scala.reflect._
 import scala.annotation.{ClassfileAnnotation, StaticAnnotation}
-import scala.language.experimental.macros
-object MacroTest {
-  def paramsOf[T]: List[String] = macro Macros.paramsOf[T]
-  def fwat[Layout,Target](layout: Layout): formidable.FormidableThisTimeBetter.Formidable[Target] = macro Macros.fwat[Layout,Target]
-}
 
-class key(s: String) extends StaticAnnotation
+object MacroTest {
+  def paramsOf[T](): List[String] = macro Macros.paramsOf[T]
+  def fwat[Layout,Target](layout: Layout): formidable.FormidableThisTimeBetter.Formidable[Target] = macro Macros.mkFormidable[Layout,Target]
+}
 
 object Macros {
 
-  def getCompanion(c: Context)(tpe: c.Type) = {
+  def getCompanion(c: blackbox.Context)(tpe: c.Type) = {
     import c.universe._
     val symTab = c.universe.asInstanceOf[reflect.internal.SymbolTable]
     val pre = tpe.asInstanceOf[symTab.Type].prefix.asInstanceOf[Type]
-    c.universe.treeBuild.mkAttributedRef(pre, tpe.typeSymbol.companionSymbol)
+    c.universe.internal.gen.mkAttributedRef(pre, tpe.typeSymbol.companion)
   }
 
-  def fwat[Layout: c.WeakTypeTag, Target: c.WeakTypeTag](c: Context)(layout: c.Expr[Layout])
+  def mkFormidable[Layout: c.WeakTypeTag, Target: c.WeakTypeTag](c: blackbox.Context)(layout: c.Expr[Layout])
     : c.Expr[formidable.FormidableThisTimeBetter.Formidable[Target]] = {
     import c.universe._
 
     val targetTpe = weakTypeTag[Target].tpe
     val layoutTpe = weakTypeTag[Layout].tpe
 
-    //println(targetTpe)
-    println("For layout...")
-    //val layoutAccessors = layoutTpe.decls.collect{
-    //  case x if x.isPublic && x.asTerm.isAccessor => { println(x); x.asTerm }
-    //}.toList
-    //println(layoutAccessors)
-    //println("QQQQQQQQQQQQQQQQQQQQQQQQ")
-    //println(targetTpe)
-    val layoutAccessors = layoutTpe.decls.map(_.asTerm).filter(_.isAccessor).toList
-    val targetAccessors = targetTpe.decls.map(_.asTerm).filter(_.isAccessor).toList
-    println(targetAccessors)
-    println(layoutAccessors)
-    val layoutNames = layoutAccessors.map(_.name.toString).toSet
-    val targetNames = targetAccessors.map(_.name.toString).toSet
+    val fields = targetTpe.decls.collectFirst {
+      case m: MethodSymbol if m.isPrimaryConstructor => m
+    }.get.paramLists.head
 
+    val layoutAccessors = layoutTpe.decls.map(_.asTerm).filter(_.isAccessor).toList
+    val layoutNames = layoutAccessors.map(_.name.toString).toSet
+    val targetNames = fields.map(_.name.toString).toSet
     val missing = targetNames.diff(layoutNames)
 
     if(missing.size > 0) {
-
       c.abort(c.enclosingPosition,s"The layout is not fully defined: Missing fields are:\n${missing.mkString("\n")}")
     }
-    println(targetTpe.typeSymbol.companion)
-    val symTab = c.universe.asInstanceOf[reflect.internal.SymbolTable]
-    println(symTab)
-    println(s"???? ${targetTpe.asInstanceOf[symTab.Type]}")
-    //val wurt = c.universe.internal.gen.mkAttributedRef(targetTpe,targetTpe.typeSymbol.companion)
-    //println(wurt)
-    //println(getCompanion(c)(targetTpe))
+
+    val companion = getCompanion(c)(targetTpe)
+
+    val magic = fields.zipWithIndex.map { case (field,idx) =>
+      val term = TermName(s"a$idx")
+      q"$layout.${layoutAccessors.find(_.name == field.name).get}.value = $term"
+    }
+
+    val bnd2 = q"$companion.unapply(inp).map { case (a0,a1) => $magic }"
+    val bnd3 = q"$companion.unapply(inp).map { case (a0,a1,a2) => $magic }"
+    val bnd4 = q"$companion.unapply(inp).map { case (a0,a1,a2,a3) => $magic }"
+    val bnd5 = q"$companion.unapply(inp).map { case (a0,a1,a2,a3,a4) => $magic }"
+    val bnd6 = q"$companion.unapply(inp).map { case (a0,a1,a2,a3,a4,a5) => $magic }"
+
     c.Expr[formidable.FormidableThisTimeBetter.Formidable[Target]](q"""
-    class LeL(binding: $layoutTpe) extends Formidable[$targetTpe]{
+    new Formidable[$targetTpe] {
       def populate(inp: $targetTpe): Unit = {
-        Thingz.unapply(inp).map { case (a1,a2) =>
-          binding.${layoutAccessors(0)}.value = a1
-          binding.${layoutAccessors(1)}.value = a2
-          println("YOLOLOBITCHEZ")
-        }
+        ${ fields.size match {
+          case 2 => bnd2
+          case 3 => bnd3
+          case 4 => bnd4
+          case 5 => bnd5
+          case 6 => bnd6
+          case _ => c.abort(c.enclosingPosition,"Unsupported Case Class Dimension")
+        }}
+      }
+
+      def construct(): $targetTpe = {
+        $companion.apply(
+          $layout.${layoutAccessors(0)}.value,
+          $layout.${layoutAccessors(1)}.value,
+          $layout.${layoutAccessors(2)}.value
+        )
       }
     }
-    new LeL($layout)
     """)
   }
 
-  def paramsOf[T: c.WeakTypeTag](c: Context): c.Expr[List[String]] = {
+  def paramsOf[T: c.WeakTypeTag](c: whitebox.Context)(): c.Expr[List[String]] = {
     import c.universe._
     val tpe = weakTypeTag[T].tpe
     println("########### TYPE SYMBOL ############")
@@ -83,19 +90,22 @@ object Macros {
     println("##### clsSymbol???? #####")
     val clsSymbol = tpe.typeSymbol.asClass
     println(clsSymbol)
+    println("#########  TERM SYMBOL ###########")
+    println(tpe.termSymbol)
+    println("#########  FUCKING FUCK FUCK ###########")
+    println(tpe.typeSymbol.isClass)
+    println(tpe.typeSymbol.companionSymbol)
 
-    val wut = tpe.declaration(nme.CONSTRUCTOR) match {
+    val wut = tpe.decl(termNames.CONSTRUCTOR) match {
       case x => {
         println("Seriously, wat")
         println(s"Sealed? ${clsSymbol.isSealed}")
         println(s"isModuleClass? ${tpe.typeSymbol.isModuleClass}")
-        println(x)
         println("-------------------------------------")
         val companion = getCompanion(c)(tpe)
-        println(companion)
         val argSyms = companion
           .tpe
-          .member(newTermName("apply"))
+          .member(TermName("apply"))
           .asMethod
           .paramLists
           .flatten
--- js/src/test/scala/formidable/Basic.scala
+++ js/src/test/scala/formidable/Basic.scala
@@ -7,22 +7,23 @@ import formidable.Fields._
 import formidable.validators.any._
 import org.scalajs.dom
 
+
 object BasicTests extends TestSuite {
+
+  case class Thingy(foo: String, bar: String)
+  case class Thingz(foo: String, renamed: String, another: String)
+  case class Basic1(inp: String)
 
   def tests = TestSuite {
     'case2 {
       import FormidableTryAgain._
 
-      case class Thingy(foo: String, bar: String)
-
+      val kill = Thingy("Foo","Bar")
       val wat = new Formidable(Mapper(
         "aaa"  -> new Basic[String](AnyString)(`type`:="text"),
         "bbb"  -> new Basic[String](AnyString)(`type`:="text")
       )(Thingy.apply)(Thingy.unapply))
-
       wat.populate(Thingy("QQQ","ZZZ"))
-      //assert(wat("aaa").asInstanceOf[dom.HTMLInputElement].value == "QQQ")
-      //assert(wat("bbb").asInstanceOf[dom.HTMLInputElement].value == "ZZZ")
     }
     'nested {
       import FormidableTryAgain._
@@ -37,21 +38,30 @@ object BasicTests extends TestSuite {
         )(Thingy.apply)(Thingy.unapply)
       )(NestIt.apply)(NestIt.unapply))
     }
+
     'again {
       import FormidableThisTimeBetter._
-      case class Thingz(foo: String, renamed: String)
-      val foo = Thingz("YOLOYOLO","BITNNHZ")
 
-      class ThingyLayout {
+      val foo = Thingz("YOLOYOLO","BITNNHZ","ANOTHER")
+
+      object ThingyLayout {
         val foo = scalatags.JsDom.tags.input(`type`:="text").render
+        val another = scalatags.JsDom.tags.input(`type`:="text").render
         val renamed = scalatags.JsDom.tags.input(`type`:="text").render
       }
 
-      val zzz = new ThingyLayout
-      val wat: Formidable[Thingz] = MacroTest.fwat[ThingyLayout,Thingz](zzz)
+      //val zzz = new ThingyLayout
+      val wat: Formidable[Thingz] = MacroTest.fwat[ThingyLayout.type,Thingz](ThingyLayout)
       wat.populate(foo)
-      println(zzz.foo.value)
-      println(zzz.renamed.value)
+      //println(zzz.foo.value)
+      //println(zzz.renamed.value)
+      //println(zzz.another.value)
+      println(ThingyLayout.another.value)
+      ThingyLayout.foo.value = "DDDDDDDDDIIIIIIIIIIEEEEEEEEEE"
+      val meh = wat.construct()
+      println(meh)
+      println(meh)
+      println(meh)
     }
   }
 }
