33b78f5638c3e7c3825db34802d7a1400fa9c318
andrew <andrew@stabtechs.com>
2015-04-02 16:55:37 -0700

    lolwat

--- js/src/main/scala/formidable/Validation.scala
+++ js/src/main/scala/formidable/Validation.scala
@@ -32,14 +32,14 @@ object Validation {
   }
 
   class ValidateNumber[N: Numeric, T]
-      (check: N => Try[T], asNumber: T => N, fromString: String => N, mods: Modifier*)
+      (check: N => Try[T], asNumber: T => N, fromString: String => Try[N], mods: Modifier*)
       (rxMods: (Var[Try[T]] => Modifier)*) extends Formidable[T] {
 
     val current: Var[Try[T]] = Var(Failure(Unitialized))
 
     lazy val input: org.scalajs.dom.html.Input = scalatags.JsDom.all.input(
       `type` := "text",
-      onkeyup := { () => current() = check(fromString(input.value))},
+      onkeyup := { () => current() = fromString(input.value).flatMap(check) },
       mods,
       rxMods.map(_(current))
     ).render
@@ -54,6 +54,6 @@ object Validation {
 
   object Validate {
     def apply[T](check: String => Try[T], asString: T => String, mods: Modifier*)(rxMods: (Var[Try[T]] => Modifier)*)  = new Validate(check,asString,mods:_*)(rxMods:_*)
-    def int[T](check: Int => Try[T], asInt: T => Int, mods: Modifier*)(rxMods: (Var[Try[T]] => Modifier)*) = new ValidateNumber[Int,T](check, asInt, _.toInt , mods:_*)(rxMods:_*)
+    def int[T](check: Int => Try[T], asInt: T => Int, mods: Modifier*)(rxMods: (Var[Try[T]] => Modifier)*) = new ValidateNumber[Int,T](check, asInt, q => Try(q.toInt) , mods:_*)(rxMods:_*)
   }
 }
